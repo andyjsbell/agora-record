a1b66f8269888268def6220ae49ad49f0d071d83
Andy Bell <andy.bell@displaynote.com>
2020-05-15 08:44:28 +0200

    structure of project

---
+++ build-node-gyp/src/build/Release/.deps/Release/obj.target/agorasdk/agora_node_ext/AgoraSdk.o.d
@@ -0,0 +1,44 @@
+cmd_Release/obj.target/agorasdk/agora_node_ext/AgoraSdk.o := g++ '-DNODE_GYP_MODULE_NAME=agorasdk' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DOPENSSL_NO_PINSHARED' '-DBUILDING_NODE_EXTENSION' -I/home/andy/.cache/node-gyp/10.18.1/include/node -I/home/andy/.cache/node-gyp/10.18.1/src -I/home/andy/.cache/node-gyp/10.18.1/deps/openssl/config -I/home/andy/.cache/node-gyp/10.18.1/deps/openssl/openssl/include -I/home/andy/.cache/node-gyp/10.18.1/deps/uv/include -I/home/andy/.cache/node-gyp/10.18.1/deps/zlib -I/home/andy/.cache/node-gyp/10.18.1/deps/v8/include -I../sdk/include -I../sdk/include/base  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++1y -MMD -MF ./Release/.deps/Release/obj.target/agorasdk/agora_node_ext/AgoraSdk.o.d.raw   -c -o Release/obj.target/agorasdk/agora_node_ext/AgoraSdk.o ../agora_node_ext/AgoraSdk.cpp
+Release/obj.target/agorasdk/agora_node_ext/AgoraSdk.o: \
+ ../agora_node_ext/AgoraSdk.cpp ../sdk/include/IAgoraLinuxSdkCommon.h \
+ ../sdk/include/IAgoraRecordingEngine.h \
+ ../sdk/include/IAgoraLinuxSdkCommon.h ../agora_node_ext/AgoraSdk.h \
+ ../agora_node_ext/node_napi_api.h \
+ /home/andy/.cache/node-gyp/10.18.1/include/node/node.h \
+ /home/andy/.cache/node-gyp/10.18.1/include/node/v8.h \
+ /home/andy/.cache/node-gyp/10.18.1/include/node/v8-version.h \
+ /home/andy/.cache/node-gyp/10.18.1/include/node/v8config.h \
+ /home/andy/.cache/node-gyp/10.18.1/include/node/v8-platform.h \
+ /home/andy/.cache/node-gyp/10.18.1/include/node/node_version.h \
+ ../agora_node_ext/node_uid.h ../sdk/include/base/atomic.h \
+ ../agora_node_ext/opt_parser.h ../agora_node_ext/node_async_queue.h \
+ /home/andy/.cache/node-gyp/10.18.1/include/node/uv.h \
+ /home/andy/.cache/node-gyp/10.18.1/include/node/uv/errno.h \
+ /home/andy/.cache/node-gyp/10.18.1/include/node/uv/version.h \
+ /home/andy/.cache/node-gyp/10.18.1/include/node/uv/unix.h \
+ /home/andy/.cache/node-gyp/10.18.1/include/node/uv/threadpool.h \
+ /home/andy/.cache/node-gyp/10.18.1/include/node/uv/linux.h \
+ ../agora_node_ext/node_log.h
+../agora_node_ext/AgoraSdk.cpp:
+../sdk/include/IAgoraLinuxSdkCommon.h:
+../sdk/include/IAgoraRecordingEngine.h:
+../sdk/include/IAgoraLinuxSdkCommon.h:
+../agora_node_ext/AgoraSdk.h:
+../agora_node_ext/node_napi_api.h:
+/home/andy/.cache/node-gyp/10.18.1/include/node/node.h:
+/home/andy/.cache/node-gyp/10.18.1/include/node/v8.h:
+/home/andy/.cache/node-gyp/10.18.1/include/node/v8-version.h:
+/home/andy/.cache/node-gyp/10.18.1/include/node/v8config.h:
+/home/andy/.cache/node-gyp/10.18.1/include/node/v8-platform.h:
+/home/andy/.cache/node-gyp/10.18.1/include/node/node_version.h:
+../agora_node_ext/node_uid.h:
+../sdk/include/base/atomic.h:
+../agora_node_ext/opt_parser.h:
+../agora_node_ext/node_async_queue.h:
+/home/andy/.cache/node-gyp/10.18.1/include/node/uv.h:
+/home/andy/.cache/node-gyp/10.18.1/include/node/uv/errno.h:
+/home/andy/.cache/node-gyp/10.18.1/include/node/uv/version.h:
+/home/andy/.cache/node-gyp/10.18.1/include/node/uv/unix.h:
+/home/andy/.cache/node-gyp/10.18.1/include/node/uv/threadpool.h:
+/home/andy/.cache/node-gyp/10.18.1/include/node/uv/linux.h:
+../agora_node_ext/node_log.h:
---
+++ build-node-gyp/src/build/Release/.deps/Release/obj.target/agorasdk/agora_node_ext/agora_node_ext.o.d
@@ -0,0 +1,38 @@
+cmd_Release/obj.target/agorasdk/agora_node_ext/agora_node_ext.o := g++ '-DNODE_GYP_MODULE_NAME=agorasdk' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DOPENSSL_NO_PINSHARED' '-DBUILDING_NODE_EXTENSION' -I/home/andy/.cache/node-gyp/10.18.1/include/node -I/home/andy/.cache/node-gyp/10.18.1/src -I/home/andy/.cache/node-gyp/10.18.1/deps/openssl/config -I/home/andy/.cache/node-gyp/10.18.1/deps/openssl/openssl/include -I/home/andy/.cache/node-gyp/10.18.1/deps/uv/include -I/home/andy/.cache/node-gyp/10.18.1/deps/zlib -I/home/andy/.cache/node-gyp/10.18.1/deps/v8/include -I../sdk/include -I../sdk/include/base  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++1y -MMD -MF ./Release/.deps/Release/obj.target/agorasdk/agora_node_ext/agora_node_ext.o.d.raw   -c -o Release/obj.target/agorasdk/agora_node_ext/agora_node_ext.o ../agora_node_ext/agora_node_ext.cpp
+Release/obj.target/agorasdk/agora_node_ext/agora_node_ext.o: \
+ ../agora_node_ext/agora_node_ext.cpp ../agora_node_ext/agora_node_ext.h \
+ ../agora_node_ext/node_log.h ../agora_node_ext/agora_node_recording.h \
+ /home/andy/.cache/node-gyp/10.18.1/include/node/node.h \
+ /home/andy/.cache/node-gyp/10.18.1/include/node/v8.h \
+ /home/andy/.cache/node-gyp/10.18.1/include/node/v8-version.h \
+ /home/andy/.cache/node-gyp/10.18.1/include/node/v8config.h \
+ /home/andy/.cache/node-gyp/10.18.1/include/node/v8-platform.h \
+ /home/andy/.cache/node-gyp/10.18.1/include/node/node_version.h \
+ /home/andy/.cache/node-gyp/10.18.1/include/node/node_object_wrap.h \
+ ../agora_node_ext/node_napi_api.h ../agora_node_ext/node_uid.h \
+ ../agora_node_ext/./AgoraSdk.h ../agora_node_ext/./node_napi_api.h \
+ ../agora_node_ext/./node_uid.h ../sdk/include/IAgoraLinuxSdkCommon.h \
+ ../sdk/include/IAgoraRecordingEngine.h \
+ ../sdk/include/IAgoraLinuxSdkCommon.h ../sdk/include/base/atomic.h \
+ ../agora_node_ext/./opt_parser.h
+../agora_node_ext/agora_node_ext.cpp:
+../agora_node_ext/agora_node_ext.h:
+../agora_node_ext/node_log.h:
+../agora_node_ext/agora_node_recording.h:
+/home/andy/.cache/node-gyp/10.18.1/include/node/node.h:
+/home/andy/.cache/node-gyp/10.18.1/include/node/v8.h:
+/home/andy/.cache/node-gyp/10.18.1/include/node/v8-version.h:
+/home/andy/.cache/node-gyp/10.18.1/include/node/v8config.h:
+/home/andy/.cache/node-gyp/10.18.1/include/node/v8-platform.h:
+/home/andy/.cache/node-gyp/10.18.1/include/node/node_version.h:
+/home/andy/.cache/node-gyp/10.18.1/include/node/node_object_wrap.h:
+../agora_node_ext/node_napi_api.h:
+../agora_node_ext/node_uid.h:
+../agora_node_ext/./AgoraSdk.h:
+../agora_node_ext/./node_napi_api.h:
+../agora_node_ext/./node_uid.h:
+../sdk/include/IAgoraLinuxSdkCommon.h:
+../sdk/include/IAgoraRecordingEngine.h:
+../sdk/include/IAgoraLinuxSdkCommon.h:
+../sdk/include/base/atomic.h:
+../agora_node_ext/./opt_parser.h:
